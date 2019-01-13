fb3c2a0693a78aa88fce389741a10e49723021c8
Jose Pablo Apú <josepabloapu@gmail.com>
2019-01-13 13:49:12 -0600

    filers desired properties when updating user

--- routes/users-me.js
+++ routes/users-me.js
@@ -11,11 +11,15 @@ module.exports.get = function (req, res) {
 module.exports.update = function (req, res) {
   User.findById(req.user._id, function (err, user) {
     if (err) return res.status(500).json({ message: err })
+
+    // bypass desired properties
     var key
     for (key in req.body) {
-      if (key == 'role') { return res.status(500).json({ message: 'Unable to change the role' }) }
-      user[key] = req.body[key] 
+      if (key == 'name' || key == 'email' || key == 'language' || key == 'currency') {
+        user[key] = req.body[key]  
+      }
     }
+
     user.save(function (err) {
       if (err) return res.status(500).json({ message: err })
       res.send(user)
